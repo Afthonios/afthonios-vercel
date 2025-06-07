e3748a81d2c59ee6abdebf88923f2df938ec786c
Afthonios <nicolai.kalb@afthonios.com>
2025-06-07 06:59:36 +0300

    Commit all remaining changes

--- src/app/[locale]/(site)/gestion-de-projet/page.tsx
+++ src/app/[locale]/(site)/gestion-de-projet/page.tsx
@@ -2,7 +2,7 @@ import { locales } from '@/i18n';
 import ProjectAcademyPage from '@/components/pages/project-academy';
 
 export function generateStaticParams(): { locale: string }[] {
-  return (locales as string[]).map((locale) => ({ locale }));
+  return Array.from(locales).map((locale) => ({ locale }));
 }
 
 export default async function Page({
