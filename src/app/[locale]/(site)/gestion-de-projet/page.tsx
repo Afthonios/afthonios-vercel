c28ccc4028b7024f595d24802d6501f915162ff3
Afthonios <nicolai.kalb@afthonios.com>
2025-06-10 06:48:10 +0300

    🐛 Fix: moved type import outside function

--- src/app/[locale]/(site)/gestion-de-projet/page.tsx
+++ src/app/[locale]/(site)/gestion-de-projet/page.tsx
@@ -2,6 +2,7 @@ import { getProjectAcademyPage } from '@/lib/directus';
 import { notFound } from 'next/navigation';
 import ProjectAcademyPage from '@/components/pages/project-academy';
 import { generateLocaleParams } from "@/lib/staticLocales";
+import type { ProjectAcademyPageProps } from '@/components/pages/project-academy';
 
 export function generateStaticParams() {
   return generateLocaleParams();
@@ -9,7 +10,6 @@ export function generateStaticParams() {
 
 export default async function Page({ params }: { params: { locale: string } }) {
   const { locale } = params;
-  import type { ProjectAcademyPageProps } from '@/components/pages/project-academy';
 
   const data: ProjectAcademyPageProps['data'] = await getProjectAcademyPage();
   if (!data) return notFound();
